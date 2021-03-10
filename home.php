6074f5c901b539babf2375b81948fd3fa45cef57
Jacob Stephens <jacob.stephens@gordon.edu>
2021-03-10 09:30:04 -0500

    working post_display_order

--- home.php
+++ home.php
@@ -14,13 +14,37 @@
 
 namespace WP_Rig\WP_Rig;
 
+use WP_Query;
+
 get_header();
 
 wp_rig()->print_styles( 'wp-rig-content', 'wp-rig-home' );
 
 ?>
 	<main id="primary" class="site-main">
-		<?php
+
+	<?php
+
+		// query
+		$the_query = new WP_Query(array(
+			'post_type'			=> 'post',
+			'posts_per_page'	=> 5,
+			'meta_key'			=> 'post_display_order',
+			'orderby'			=> 'meta_value',
+			'order'				=> 'DESC'
+		));
+
+		if( $the_query->have_posts() ):
+
+			while( $the_query->have_posts() ) : $the_query->the_post();
+				get_template_part( 'template-parts/content/entry_home', get_post_type() );
+			endwhile;
+
+		endif;
+
+	?>
+
+	<?php
 
 		if ( have_posts() ) {
 
