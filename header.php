20ccb9a8c6b35ffd1f722d14d8673d024e9cedbc
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-09-23 18:07:37 -0400

    readme update

--- header.php
+++ header.php
@@ -29,6 +29,15 @@ namespace WP_Rig\WP_Rig;
 	?>
 
 	<?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-L52ECXYRT8"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-L52ECXYRT8');
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
